c5c6ebad69d6f726c59c3c5b3a38fcaa39a1f9a4
Samuel Bishop <sjbc35@mail.missouri.edu>
2022-07-30 20:28:14 -0700

    Add rotation to the control

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 import { WebGLRenderer, PerspectiveCamera, Scene, Vector3, Clock } from 'three';
+import { OrbitControls } from './OrbitControls';
 import SeedScene from './objects/Scene.js';
 
 const scene = new Scene();
-const camera = new PerspectiveCamera();
+const { innerHeight, innerWidth } = window;
+const camera = new PerspectiveCamera(45, innerWidth / innerHeight, 1, 1000);
 const renderer = new WebGLRenderer({antialias: true});
 const seedScene = new SeedScene();
-
-// Importing me
-
+const controls = new OrbitControls( camera, renderer.domElement );
 
 // scene
 scene.add(seedScene);
+
+// Setting up the controls
+controls.enableZoom = false;
+controls.keys = {
+	LEFT: 'ArrowLeft', //left arrow
+	UP: 'ArrowUp', // up arrow
+	RIGHT: 'ArrowRight', // right arrow
+	BOTTOM: 'ArrowDown' // down arrow
+}
+controls.maxPolarAngle = Math.PI/1.3;
+controls.minPolarAngle = Math.PI/4;
+// controls.position.set(0, 1.7, 3);
 
 // camera
 // 2 definetly z
 // 1 is back from the guy
 // 3 is to the right of the guy
-camera.position.set(0,1.7,3);
-camera.lookAt(new Vector3(0,1,0));
+const ptToLookAt = new Vector3(0, 1, 0);
+/// const cameraPositionPt = new Vector3(0, 1.7, 3);
+controls.target.set(0, 1, 0);
+camera.position.set(0, 1.7, 3);
+camera.lookAt(ptToLookAt);
+controls.update();
 
 // renderer
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -36,6 +51,7 @@ const onAnimationFrameHandler = (timeStamp) => {
   seedScene.update && seedScene.update(timeStamp);
   if ( seedScene.children[0].mixer ) seedScene.children[0].mixer.update( clock.getDelta() );
   window.requestAnimationFrame(onAnimationFrameHandler);
+  // controls.update();
 }
 window.requestAnimationFrame(onAnimationFrameHandler);
 
@@ -51,25 +67,15 @@ window.addEventListener('resize', windowResizeHanlder);
 
 // dom
 document.body.style.margin = 0;
-// document.body.removeChild(2)
 if (document.body.children[2]){
   document.body.removeChild(document.body.children[2])
 }
 document.body.appendChild( renderer.domElement );
 
-// if (document.getElementById("root").children){
-//   document.body.appendChild( renderer.domElement );
-// }
-// document.body[1] = renderer.domElement;
-
 
 function App() {
   return (
     <body></body>
-    // <div class="center">
-    //   <div class="titleText">Good morning, cutie.</div>
-    //   <div class="titleText">Here's a flower for you</div>
-    // </div>
   );
 }
 
